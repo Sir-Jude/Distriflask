882bd834f2c0dfa3d9b619bdc9589bac3a5aaa6a
Giulio Arpaia <arpaia@jpk.com>
2023-11-07 16:32:23 +0100

    start to create the logic/template for admin and user page

--- project.py
+++ project.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+@app.route('/home/')
+def home_page():
+    return render_template("welcome.html")
 
+@app.route('/admin/')
+def admin():
+    return render_template("admin.html")
+
+@app.route('/user/<name>/')
+def user(name):
+    return render_template("user.html", name=name)
