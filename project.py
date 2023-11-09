4b485eca3a8420aae6755bd5f383ef8a11545ed4
Giulio Arpaia <arpaia@jpk.com>
2023-11-09 14:15:16 +0100

    Create the Register and login feature - bug: when user logs in, nothing happensCC

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user, login_user
 import os
 from dotenv import load_dotenv
 
@@ -17,8 +17,8 @@ from wtforms.validators import InputRequired, Length, ValidationError
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 db = SQLAlchemy(app)
 admin = Admin(app)
 login_manager = LoginManager(app)
@@ -40,20 +40,15 @@ class User(db.Model):
     # region_id = db.Column pass # multiple regions
     # devices = db.Column # Only for customers, just one device (one to one)
 
+@app.route("/")
+@app.route("/home/", methods=["GET"])
+def home_page():
+    username = None
+    if current_user.is_authenticated:
+        username = current_user.username
+    return render_template("welcome.html", username=username)
 
 admin.add_view(ModelView(User, db.session))
-
-
-@app.route("/login/", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    return render_template("registration/login.html", form=form)
-
-
-@app.route("/register/", methods=["GET", "POST"])
-def register():
-    form = RegisterForm()
-    return render_template("registration/register.html", form=form)
 
 
 class RegisterForm(FlaskForm):
@@ -65,14 +60,29 @@ class RegisterForm(FlaskForm):
         validators=[InputRequired(), Length(min=8, max=20)],
         render_kw={"placeholder": "Password"},
     )
-    submit = SubmitField("Register")
+    submit = SubmitField("Register", render_kw={"class": "btn btn-primary"})
     
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
+    def validate_username(self, field):
+        existing_user_username = User.query.filter_by(username=field.data).first()
         if existing_user_username:
             raise ValidationError(
                 "This username already exists. Please choose a different one."
             )
+
+
+@app.route("/register/", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return redirect(url_for ("login"))
+    
+    return render_template("registration/register.html", form=form)
+
 
 class LoginForm(FlaskForm):
     username = StringField(
@@ -83,7 +93,7 @@ class LoginForm(FlaskForm):
         validators=[InputRequired(), Length(min=8, max=20)],
         render_kw={"placeholder": "Password"},
     )
-    submit = SubmitField("Login")
+    submit = SubmitField("Login", render_kw={"class": "btn btn-primary"})
     
     def validate_username(self, username):
         existing_user_username = User.query.filter_by(username=username.data).first()
@@ -92,11 +102,22 @@ class LoginForm(FlaskForm):
                 "This username already exists. Please choose a different one."
             )
 
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Assuming User.authenticate is a method that checks the username and password
+        user = User.authenticate(username=form.username.data, password=form.password.data)
 
-@app.route("/")
-@app.route("/home/")
-def home_page():
-    return render_template("welcome.html")
+        if user:
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))  # Redirect to the dashboard or another page
+        else:
+            flash('Invalid username or password', 'danger')
+
+    return render_template("registration/login.html", form=form)
+    return render_template("registration/login.html", form=form)
 
 
 @app.route("/user/<name>/")
