341096808be7072b7129875752aaf57ba6ded396
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-11-14 11:52:35 +0100

    Heavily remake of the code: now only Admin can register new users, the password needs to be inserted twice and is hashed

--- project.py
+++ project.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, redirect, url_for, flash
+# Issues:
+# - Move the button "Register" from the normal homepage to the admin homepage
+# - After i have register a new user, I want to be redirect to the list of users in admin, not the admin dashboard.
+
+
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_login import (
     UserMixin,
     LoginManager,
@@ -14,10 +20,9 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy_utils import UUIDType
 import uuid
-from flask_admin.contrib.sqla import ModelView
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError, EqualTo
+from wtforms.validators import InputRequired, Length, DataRequired, EqualTo
 from flask_migrate import Migrate
 
 
@@ -32,9 +37,13 @@ admin = Admin(app)
 login_manager = LoginManager(app)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
+@app.route("/")
+@app.route("/home/", methods=["GET"])
+def home_page():
+    username = None
+    if current_user.is_authenticated:
+        username = current_user.username
+    return render_template("welcome.html", username=username)
 
 
 class User(db.Model, UserMixin):
@@ -43,7 +52,7 @@ class User(db.Model, UserMixin):
         UUIDType(binary=False), primary_key=True, default=uuid.uuid4, unique=True
     )
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password_hash = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
     # role = db.Column pass # multiple roles
     # region_id = db.Column pass # multiple regions
     # devices = db.Column # Only for customers, just one device (one to one)
@@ -70,57 +79,40 @@ class User(db.Model, UserMixin):
         return str(self.user_id)
 
 
-@app.route("/")
-@app.route("/home/", methods=["GET"])
-def home_page():
-    username = None
-    if current_user.is_authenticated:
-        username = current_user.username
-    return render_template("welcome.html", username=username)
+class UserAdminView(ModelView):
+    column_list = ("username", "password_hash")  # Exclude 'user_id' from the list view
+    form_excluded_columns = ("user_id",)  # Exclude 'user_id' from the detail view
+    column_formatters = {
+        "password_hash": lambda v, c, m, p: "●●●●●●●●" if m.password_hash else ""
+    }
+
+    def scaffold_form(self):
+        form_class = super(UserAdminView, self).scaffold_form()
+        form_class.password = PasswordField(
+            "Password",
+            [
+                DataRequired(),
+                EqualTo("password_confirm", message="Passwords must match"),
+            ],
+        )
+        form_class.password_confirm = PasswordField("Confirm Password")
+        return form_class
+
+    def on_model_change(self, form, model, is_created):
+        if "password" in form.data and "password_confirm" in form.data:
+            if form.data["password"] == form.data["password_confirm"]:
+                if form.data["password"]:
+                    model.password = form.data["password"]
+
+        super(UserAdminView, self).on_model_change(form, model, is_created)
 
 
-admin.add_view(ModelView(User, db.session))
+admin.add_view(UserAdminView(User, db.session))
 
 
-class RegisterForm(FlaskForm):
-    username = StringField(
-        validators=[InputRequired(), Length(min=4, max=20)],
-        render_kw={"placeholder": "Username"},
-    )
-    password = PasswordField(
-        validators=[
-            InputRequired(),
-            Length(min=8, max=20),
-            EqualTo("password_2", message="Passwords must match!"),
-        ],
-        render_kw={"placeholder": "Password"},
-    )
-    password_2 = PasswordField(
-        validators=[InputRequired()],
-        render_kw={"placeholder": "Confirm Password"},
-    )
-    submit = SubmitField("Register", render_kw={"class": "btn btn-primary"})
-
-    def validate_username(self, field):
-        existing_user_username = User.query.filter_by(username=field.data).first()
-        if existing_user_username:
-            raise ValidationError(
-                "This username already exists. Please choose a different one."
-            )
-
-
-@app.route("/register/", methods=["GET", "POST"])
-def register():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        new_user = User(username=form.username.data, password=form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for("login"))
-
-    return render_template("registration/register.html", form=form)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 class LoginForm(FlaskForm):
@@ -145,12 +137,16 @@ def login():
 
         if user:
             login_user(user)
-            flash("User successufully logged", "success")
             return redirect(url_for("user", username=form.username.data))
-        else:
-            flash("Invalid username or password", "danger")
 
     return render_template("registration/login.html", form=form)
+
+
+@app.route("/user/<username>/")
+def user(username):
+    # To be substituted with a database...
+    devices = ["device_1", "device_2", "device_3", "device_4", "..."]
+    return render_template("user.html", username=username, devices=devices)
 
 
 @app.route("/logout/", methods=["GET", "POST"])
@@ -160,15 +156,10 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/user/<username>/")
-def user(username):
-    # To be substituted with a database...
-    devices = ["device_1", "device_2", "device_3", "device_4", "..."]
-    return render_template("user.html", username=username, devices=devices)
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
+
 
 @app.errorhandler(500)
 def page_not_found(e):
