7bf68d6939eece178a32e891ec3dffba35f3b394
Giulio Arpaia <arpaia@jpk.com>
2023-11-28 18:02:11 +0100

    Succeeded in implementing Flask security, but templates need to be modified and joined with old_project

--- project.py
+++ project.py
@@ -45,6 +45,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 
+
+@app.route("/")
+def home_page():
+    pass
+
+
 roles_users_table = db.Table(
     'roles_users',
     db.Column('users_id', db.Integer(), 
@@ -57,12 +63,12 @@ class Users(db.Model, UserMixin):
     user_id = db.Column(
         UUIDType(binary=False), primary_key=True, default=uuid.uuid4, unique=True
     )
-    email = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(80))
     device = db.Column(db.String(200), nullable=True)
     active = db.Column(db.Boolean())
     roles = db.relationship('Roles', secondary=roles_users_table, backref='user', lazy=True)
-    fs_uniquifier = db.Column(db.String(64), unique=True, name='unique_fs_uniquifier_constraint')
+    fs_uniquifier = db.Column(db.String(64), unique=True, nullable=True, name='unique_fs_uniquifier_constraint')
 
 class Roles(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
@@ -73,17 +79,16 @@ class Roles(db.Model, RoleMixin):
 user_datastore = SQLAlchemyUserDatastore(db, Users, Roles)
 security = Security(app, user_datastore)
 
+# @app.before_request
+# def create_user():
+#     existing_user = user_datastore.find_user(email='admin')
+#     if not existing_user:
+#         first_user = user_datastore.create_user(email='admin', password='12345678')
+#         user_datastore.toggle_active(first_user)
+#         db.session.commit()
 
-@app.before_request
-def create_user():
-    existing_user = user_datastore.find_user(email='admin')
-    if not existing_user:
-        first_user = user_datastore.create_user(email='admin', password='12345678')
-        user_datastore.toggle_active(first_user)
-        db.session.commit()
-
-
-class UserModelView(ModelView):
+class UserAdminView(ModelView):
+    column_exclude_list = ["password", "fs_uniquifier"]
     def is_accessible(self):
         return (
             current_user.is_active and
@@ -94,20 +99,75 @@ class UserModelView(ModelView):
         if not self.is_accessible():
             return redirect(url_for('security.login'))
 
-admin.add_view(UserModelView(Users, db.session))
+admin.add_view(UserAdminView(Users, db.session))
 
 @security.context_processor
 def security_context_processor():
     return dict(
         admin_base_template = admin.base_template,
         admin_view = admin.index_view,
-        get_url = url_for,
-        h = admin_helpers
+        h = admin_helpers,
+        get_url = url_for
     )
 
-@app.route("/")
-def home_page():
-    pass
+# # Flask_login stuff
+# login_manager = LoginManager(app)
+# login_manager.login_view = "login"
+
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return Users.query.get(user_id)
+
+
+# class LoginForm(FlaskForm):
+#     email = StringField(
+#         validators=[InputRequired(), Length(min=4, max=20)],
+#         render_kw={"placeholder": "Username"},
+#     )
+#     password = PasswordField(
+#         validators=[InputRequired(), Length(min=8, max=20)],
+#         render_kw={"placeholder": "Password"},
+#     )
+#     submit = SubmitField("Login", render_kw={"class": "btn btn-primary"})
+
+
+# @app.route("/login/", methods=["GET", "POST"])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = Users.query.filter_by(email=form.email.data.lower()).first()
+#         if user:
+#             if bcrypt.check_password(user.password, form.password.data):
+#                 login_user(user)
+#                 flash("Logged in successfully!")
+#                 return redirect(url_for("user", username=user.username))
+#             else:
+#                 flash("Wrong password - Try Again...")
+#         else:
+#             flash("This username does not exist - Try again...")
+#     return render_template("login.html", form=form)
+
+
+# @app.route("/user/<username>/", methods=["GET", "POST"])
+# @login_required
+# def user(username):
+#     if current_user.username != username:
+#         return render_template("errors/403.html"), 403
+
+#     # To be substituted with a database...
+#     devices = current_user.device
+#     return render_template("user.html", username=username, devices=devices)
+
+
+# @app.route("/logout/", methods=["GET", "POST"])
+# @login_required
+# def logout():
+#     logout_user()
+    
+#     flash("You have been logged out.")
+#     return redirect(url_for("login"))
+
 
 @app.errorhandler(403)
 def page_not_found(e):
--- routes.py
+++ routes.py
@@ -3,5 +3,6 @@ from flask import Blueprint, render_template
 routes = Blueprint("routes", __name__)
 
 @routes.route('/')
+@routes.route("/home/", methods=["GET"])
 def default():
     return render_template('index.html')
