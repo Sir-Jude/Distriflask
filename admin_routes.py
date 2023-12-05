bf7267e43d459fc40467b1fa2f08ce071db3b931
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-12-05 13:52:54 +0100

    Separate models from main project

--- admin_routes.py
+++ admin_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-routes = Blueprint("routes", __name__)
+routes = Blueprint("admin_routes", __name__)
 
 @routes.route('/')
 @routes.route("/home/", methods=["GET"])
