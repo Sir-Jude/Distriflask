0b39024c8ef32c92c461b7529333cbf66748b580
Giulio Arpaia <arpaia@jpk.com>
2023-12-19 09:31:40 +0100

    Fix bug new users cannot log into admin, but cannot yet log into customer website

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import RoleMixin, UserMixin
-from sqlalchemy_utils import UUIDType
+from sqlalchemy import event
 import uuid
 
 
@@ -15,7 +15,7 @@ roles_users_table = db.Table(
 
 
 class Users(db.Model, UserMixin):
-    user_id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, index=True)
     password = db.Column(db.String(80))
     device = db.Column(db.String(200), nullable=True)
@@ -26,7 +26,7 @@ class Users(db.Model, UserMixin):
     fs_uniquifier = db.Column(
         db.String(64),
         unique=True,
-        nullable=True,
+        nullable=False,
         name="unique_fs_uniquifier_constraint",
     )
 
@@ -35,6 +35,11 @@ class Roles(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
-
+    
     def __str__(self):
         return self.name
+
+@event.listens_for(Users, 'before_insert')
+def before_insert_listener(mapper, connection, target):
+    if target.fs_uniquifier is None:
+        target.fs_uniquifier = str(uuid.uuid4())
