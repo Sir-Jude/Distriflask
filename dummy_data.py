fc7eb2be48fe4bff08019af57352d15b04a7fee0
Giulio Arpaia <giulio.arpaia@gmail.com>
2023-12-21 12:04:02 +0100

    Add a script which create some dummy users and assign them a role

--- dummy_data.py
+++ dummy_data.py
@@ -1,56 +1,91 @@
 from project import app
 from models import db, Users, Roles
-from flask_security import SQLAlchemyUserDatastore
+from sqlalchemy import text
+from flask_security import SQLAlchemyUserDatastore, hash_password
+from random import choice
 from faker import Faker
 
 user_datastore = SQLAlchemyUserDatastore(db, Users, Roles)
 fake = Faker()
+N_USERS=50
 ROLES=[
-    "test",
-    "test2",
-    "customer",
     "administrator",
+    "customer",
     "sales",
     "production",
     "application",
     "software",
 ]
 
-def run():
+ROLE_CHOICE = [
+    "customer",
+    "sales",
+    "production",
+    "application",
+    "software",
+]
+
+def roles_creation():
     with app.app_context():
-        Roles.query.delete()        
-        print("Deleting all Roles...recreating!")
-
         for role_name in ROLES:
-            new_role = Roles(
-                name = role_name,
-                description = f"{role_name} role"
-            )
-            db.session.add(new_role)
-            
+            existing_role = Roles.query.filter_by(name=role_name).first()
+            if existing_role is None:
+                new_role = Roles(
+                    name=role_name,
+                    description=f"{role_name} role"
+                )
+                db.session.add(new_role)
+                print(f'Role "{new_role.name}" has beeen created')
+
         db.session.commit()
-        
-# @app.before_request
-# def create_user():
-#     with app.app_context():
-#         user_datastore = SQLAlchemyUserDatastore(db, Users, Roles)
-#         existing_user = user_datastore.find_user(username="admin")
-#         if not existing_user:
-#             first_user = user_datastore.create_user(username="admin", password="12345678")
-#             user_datastore.activate_user(first_user)
-#             db.session.commit()
 
-#             # Create the 'administrator' role if it doesn't exist
-#             admin_role = Roles.query.filter_by(name="administrator").first()
-#             if not admin_role:
-#                 admin_role = Roles(name="administrator")
-#                 db.session.add(admin_role)
-#                 db.session.commit()
 
-#             # Assign the 'administrator' role to the 'admin' user
-#             user_datastore.add_role_to_user(first_user, admin_role)
-#             db.session.commit()
+def first_user():
+    with app.app_context():
+        user_datastore = SQLAlchemyUserDatastore(db, Users, Roles)
+        existing_user = user_datastore.find_user(username="admin")
+        if not existing_user:
+            first_user = user_datastore.create_user(username="admin", password="12345678")
+            user_datastore.activate_user(first_user)
+            db.session.commit()
+        else:
+            first_user = user_datastore.delete_user(existing_user)
+            db.session.commit()
+            
+            first_user = user_datastore.create_user(username="admin", password="12345678")
+            user_datastore.activate_user(first_user)
+            db.session.commit()
+            
+            # Create the 'administrator' role if it doesn't exist
+            admin_role = Roles.query.filter_by(name="administrator", description="administrator role").first()
+            if not admin_role:
+                admin_role = Roles(name="administrator", description="administrator role")
+                db.session.add(admin_role)
+                db.session.commit()
+
+            # Assign the 'administrator' role to the 'admin' user
+            admin_role = Roles.query.filter_by(name="administrator").first()
+            user_datastore.add_role_to_user(first_user, admin_role)
+            db.session.commit()
+
+def dummy_users():
+    with app.app_context():
+        user_datastore = SQLAlchemyUserDatastore(db, Users, Roles)
+        for user in range(N_USERS):
+            ROLE=choice(ROLE_CHOICE)
+            new_user = user_datastore.create_user(
+                username = fake.name(),
+                password = hash_password("12345678"),
+                device = f"Software {user} - Mod. 1.0",
+                active = True,
+                roles = [ROLE],
+            )
+            db.session.add(new_user)
+            print(f'User "{new_user.username}" has been created.')
         
+        db.session.commit()    
+
 if __name__ == "__main__":
-    run()
-        
+    roles_creation()
+    first_user()
+    dummy_users()
