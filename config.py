f90c49b8f4edbfba1228de1b87291c5d8abb7403
Giulio Arpaia <31657930+Sir-Jude@users.noreply.github.com>
2023-12-29 13:50:56 +0100

    Add SECURITY_USER_IDENTITY_ATTRIBUTES to  config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+from flask_security import uia_username_mapper
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
@@ -12,7 +14,7 @@ class Config:
     SECURITY_POST_LOGOUT_VIEW = "/admin/"
     SECURITY_POST_REGISTER_VIEW = "/admin/"
     SECURITY_REGISTERABLE = True
+    # NOT remove! Flask is not able to create new users without
     SECURITY_REGISTER_URL = "/admin/users/new/"
-    
-    
-    
+    # Allow registration with email, but login only with username
+    SECURITY_USER_IDENTITY_ATTRIBUTES = [{"username": {"mapper": uia_username_mapper}}]
