92b153a848644f6b0eadaee4b668d0f5c05748d5
Giulio Arpaia <31657930+Sir-Jude@users.noreply.github.com>
2023-12-29 13:37:39 +0100

    Improve dummy_data script

--- dummy_data.py
+++ dummy_data.py
@@ -1,5 +1,6 @@
-from app import app
-from models import db, Users, Roles
+from app import create_app
+from app.extensions import db
+from app.models import Users, Roles
 import subprocess
 import shutil
 from flask_security import SQLAlchemyUserDatastore, hash_password
@@ -8,8 +9,8 @@ from faker import Faker
 
 user_datastore = SQLAlchemyUserDatastore(db, Users, Roles)
 fake = Faker()
-N_USERS=50
-ROLES=[
+N_USERS = 50
+ROLES = [
     "administrator",
     "customer",
     "sales",
@@ -29,54 +30,54 @@ def delete_folders():
         except FileNotFoundError:
             print(f"Folder '{folder}' not found.")
 
+
 # Function to perform database setup tasks
 def setup_database():
-    
     # Log into the shell and execute commands
     subprocess.run(
         ["flask", "shell"],
-        input="from models import db\n" "db.create_all()\n" "exit()\n",
-        text=True
+        input="from app.extensions import db\n" "db.create_all()\n" "exit()\n",
+        text=True,
     )
-    
+
     # Initiating and migrating the database
     subprocess.run(["flask", "db", "init"])
     subprocess.run(["flask", "db", "migrate"])
 
 
 def roles_creation():
+    app = create_app()
     with app.app_context():
         for role_name in ROLES:
             existing_role = Roles.query.filter_by(name=role_name).first()
             if existing_role is None:
-                new_role = Roles(
-                    name=role_name,
-                    description=f"{role_name} role"
-                )
+                new_role = Roles(name=role_name, description=f"{role_name} role")
                 db.session.add(new_role)
                 print(f'Role "{new_role.name}" has beeen created')
 
         db.session.commit()
-        
+
 
 def dummy_users():
+    app = create_app()
     with app.app_context():
         for number in range(N_USERS):
             new_user = Users(
-                username = fake.name(),
-                password = hash_password("12345678"),
-                device = f"Software {number} - Mod. 1.0",
-                active = True,
+                username=fake.name(),
+                password=hash_password("12345678"),
+                device=f"Software {number} - Mod. 1.0",
+                active=True,
             )
             db.session.add(new_user)
-            
+
             role_name = choice(ROLES)
             new_role = Roles.query.filter_by(name=role_name).first()
             new_user.roles.append(new_role)
-            
+
             print(f'User "{new_user.username}" has been created.')
-        
-        db.session.commit()    
+
+        db.session.commit()
+
 
 if __name__ == "__main__":
     delete_folders()
