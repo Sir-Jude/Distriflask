d1442720f43ee90e46d657c97529441d1ef120d4
Giulio Arpaia <31657930+Sir-Jude@users.noreply.github.com>
2024-01-02 17:01:56 +0100

    Clean __init__.py and adjust it to modifications in models.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,17 @@ from app.extensions import db, login_manager
 from flask_migrate import Migrate
 
 
+# Imports from otehr files
+from app.errors import register_error_handlers
+from app.models import Users, Roles, user_datastore
+from app.views.customers import customers
+from app.views.admin_pages import admin_pages
+from app.forms import ExtendedRegisterForm, ExtendedLoginForm
+
+
 # Imports for Flask security
 from flask_security import (
     current_user,
-    hash_password,
     Security,
 )
 
@@ -22,14 +29,6 @@ from flask_security import (
 # Imports for Admin page
 from flask_admin import Admin, helpers as admin_helpers
 from flask_admin.contrib.sqla import ModelView
-
-
-# Imports from otehr files
-from app.errors import register_error_handlers
-from app.models import Users, Roles, user_datastore
-from app.views.customers import customers
-from app.views.admin_pages import admin_pages
-from app.forms import ExtendedRegisterForm, ExtendedLoginForm
 
 
 def create_app(config_class=Config):
@@ -90,10 +89,11 @@ def create_app(config_class=Config):
             db.session.commit()
 
     class UserAdminView(ModelView):
-        column_list = ("username", "device", "active", "roles")
+        column_list = ("username", "devices", "version", "active", "roles")
         column_sortable_list = (
             "username",
-            "device",
+            "devices",
+            ("version", "releases.main_version"),
             "active",
             ("roles", "roles.name"),  # Make 'roles' sortable
         )
