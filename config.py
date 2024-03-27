98c1375c5bef97979e5baef1b1e3eb7a612eb762
Giulio Arpaia <giulio.arpaia@gmail.com>
2024-03-27 10:07:35 +0100

    Fix bug preventing matching of 'username' field between the UserAdminView and ExtendedRegisterForm

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
+# Use this function to map a username to an identity
+from flask_security import uia_username_mapper
 
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,8 +17,8 @@ class Config:
     SECURITY_POST_LOGOUT_VIEW = "/"
     SECURITY_POST_REGISTER_VIEW = "/admin/"
     SECURITY_REGISTERABLE = True
-    # DO NOT REMOVE - This line allows login with username
-    SECURITY_USERNAME_ENABLE = True
+    # DO NOT REMOVE - This allows register and login with username
+    SECURITY_USER_IDENTITY_ATTRIBUTES = [{"username": {"mapper": uia_username_mapper}}]
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "uploads/"
