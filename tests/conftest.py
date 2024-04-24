dcc02f16daa808c98b6522e7e146401248a55315
Giulio Arpaia <giulio.arpaia@gmail.com>
2024-04-24 10:36:12 +0200

    Improve conftest and add logout tests for admin and customer

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 # Pytest's configuration file
+from app.models import User, Role
+from flask_security import hash_password
+
 import pytest
 from pathlib import Path
 
@@ -16,7 +19,7 @@ def app():
     """
     This fixture creates a Flask application instance with a specific
     configuration class for testing purposes. It then sets up the application
-    context, creates necessary database tables and roles. After
+    context, creates necessary database tables, roles, and regions. After
     yielding the application instance to the test function, it performs
     teardown actions by removing the test database file.
     """
@@ -28,8 +31,12 @@ def app():
 
         create_roles(app=app)
 
+    # Provide the application instance to the test function
+    # and allow additional actions to be performed
     yield app
 
+    # Perform teardown actions after test function completes
+    # (Remove SQLite database file used for testing)
     full_path = Path.cwd() / "instance" / "test_db.sqlite3"
     os.remove(full_path)
 
@@ -54,3 +61,73 @@ def runner(app):
     interactions and test their behavior.
     """
     return app.test_cli_runner()
+
+
+@pytest.fixture()
+def admin_login(client):
+    """
+    This fixture sets up a test environment for an admin user login.
+    It creates a new admin user, logs them in, and yields the test client,
+    admin username, and admin password.
+    """
+    app = create_app(config_class=TestConfig)
+    with app.app_context():
+        with client:
+            new_admin = User(
+                username="test_admin",
+                password=hash_password("12345678"),
+                roles=[Role.query.filter_by(name="administrator").first()],
+                active=True,
+            )
+
+            db.session.add(new_admin)
+            db.session.commit()
+
+            admin = User.query.filter_by(username="test_admin").first()
+            admin_username = admin.username
+            admin_password = "12345678"
+            response = client.post(
+                "/login",
+                data=dict(
+                    username=admin_username,
+                    password=admin_password,
+                ),
+            )
+
+            yield client, admin_username, admin_password
+
+
+@pytest.fixture()
+def customer_login(client):
+    """
+    This fixture sets up a test environment for a customer user login.
+    It creates a new customer user, logs them in, and yields the test client,
+    customer username, and customer password.
+    """
+    app = create_app(config_class=TestConfig)
+    with app.app_context():
+        with client:
+            new_customer = User(
+                username="test_customer",
+                password=hash_password("12345678"),
+                roles=[Role.query.filter_by(name="customer").first()],
+                active=True,
+            )
+
+            db.session.add(new_customer)
+            db.session.commit()
+
+            # Query users with roles containing "customer"
+            customer = (
+                User.query.join(User.roles).filter(Role.name == "customer").first()
+            )
+
+            # Loop through each customer and test login
+            customer_username = customer.username
+            customer_password = "12345678"
+            response = client.post(
+                "/customer_login",
+                data=dict(username=customer_username, password=customer_password),
+            )
+
+            yield client, customer_username, customer_password
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,65 +1,40 @@
-from app.models import User, Role
-from app.extensions import db
-from config import TestConfig
-from app import create_app
-from flask_security import hash_password
+def test_admin_login(admin_login):
+    client, admin_username, admin_password = admin_login
+
+    response = client.post(
+        "/login",
+        data=dict(
+            username=admin_username,
+            password=admin_password,
+        ),
+    )
+
+    assert response.status_code == 302
+    assert response.location == "/admin/"
 
 
-def test_admin_login(client):
-    app = create_app(config_class=TestConfig)
-    with app.app_context():
-        with client:
-            new_admin = User(
-                username="test_admin",
-                password=hash_password("12345678"),
-                roles=[Role.query.filter_by(name="administrator").first()],
-                active=True,
-            )
+def test_customer_login(customer_login):
+    client, customer_username, customer_password = customer_login
+    response = client.post(
+        "/customer_login",
+        data=dict(username=customer_username, password=customer_password),
+    )
 
-            db.session.add(new_admin)
-            db.session.commit()
-
-            admin = User.query.filter_by(username="test_admin").first()
-            test_username = admin.username
-            test_password = "12345678"
-            response = client.post(
-                "/login",
-                data=dict(
-                    username=test_username,
-                    password=test_password,
-                ),
-            )
-
-        assert response.status_code == 302
-        assert response.location == "/admin/"
+    assert response.status_code == 302
+    assert response.location == f"/customer/{customer_username}/"
 
 
-def test_customer_login(client):
-    app = create_app(config_class=TestConfig)
-    with app.app_context():
-        with client:
-            new_customer = User(
-                username="test_customer",
-                password=hash_password("12345678"),
-                roles=[Role.query.filter_by(name="customer").first()],
-                active=True,
-            )
+def test_admin_logout(admin_login):
+    client = admin_login[0]
+    response = client.get("/logout")
 
-            db.session.add(new_customer)
-            db.session.commit()
+    assert response.status_code == 302
+    assert response.location == "/"
 
-            # Query users with roles containing "customer"
-            customer = (
-                User.query.join(User.roles).filter(Role.name == "customer").first()
-            )
 
-            # Loop through each customer and test login
-            test_username = customer.username
-            test_password = "12345678"
-            response = client.post(
-                "/customer_login",
-                data=dict(username=test_username, password=test_password),
-            )
+def test_customer_logout(customer_login):
+    client = customer_login[0]
+    response = client.get("/logout")
 
-        assert response.status_code == 302
-        assert response.location == f"/customer/{test_username}/"
+    assert response.status_code == 302
+    assert response.location == "/"
