d72a1b95f0a92a50800cdd43996bf9ade5c1b6fd
Giulio Arpaia <giulio.arpaia@gmail.com>
2024-09-15 14:11:20 +0200

    Add test_course_selection (overall coverage -> 70%)

--- tests/test_tab_upload.py
+++ tests/test_tab_upload.py
@@ -33,3 +33,32 @@ def test_handle_view_redirects_to_login(client):
     # Assert that the response is a redirect to the login route
     assert response.status_code == 302
     assert response.location == "/login"
+    
+def test_course_selection(admin_login, setup_course_and_exercise_data):
+    """
+    Test course selection and session storage.
+    """
+    client, _ = admin_login
+
+    # Simulate form submission to select a course
+    response = client.post(
+        "/admin/upload_admin/admin/upload/",
+        data={
+            "courses": "Test Course",
+            "select": "Submit",
+        },
+        follow_redirects=True
+    )
+
+    # Check if the session contains the selected course
+    # "session_transaction()" is a method provided by Flask’s test_client()
+    # It allows to access, inspect/modify values stored in the session during a test
+    with client.session_transaction() as sess:
+        # Retrieve value associated with key "selected_course" from session (a dictionary)
+        assert sess.get("selected_course") == "Test Course"
+
+    # Check for the flash message
+    assert b"Course Test Course selected." in response.data
+
+    # Verify the response status code
+    assert response.status_code == 200
